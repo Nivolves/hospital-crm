8b5f17f55e49b5823a3fded2e06f0090041f96e5
Danil Dubovik <d.dubovyk@nodeart.io>
2020-11-01 23:14:30 +0200

    add: patients delete

--- src/components/PatientsTable/PatientsTable.tsx
+++ src/components/PatientsTable/PatientsTable.tsx
@@ -1,28 +1,49 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import { Table } from 'antd';
 
 import { useRootData } from '../../hooks/useRootData';
 
-import { TABLE_COLUMNS } from '../../constants/TableColumns';
+import { IPatient } from '../../Types/Common';
+
+import { BASE_URL, PATIENT } from '../../constants/API';
+
+const { Column } = Table;
 
 const PatientsTable: React.FC = (): JSX.Element => {
-  const { patients } = useRootData(({ patients }) => ({
+  const { patients, setPatients } = useRootData(({ patients, setPatients }) => ({
     patients: patients.get(),
+    setPatients
   }));
 
   const history = useHistory();
 
+  const handleDeletePatient = useCallback(
+    (id) => {
+      fetch(`${BASE_URL}${PATIENT}/delete/${id}`)
+        .then(() => setPatients(patients.filter(({PatientID}) => PatientID !== id)))
+        .catch(err => console.error(err));
+    },
+    [patients, setPatients],
+  );
+
   return (
     <Table
-      onRow={row => ({
-        onClick: () => history.push(`/doctor/patient/${row.PatientID}`),
-      })}
       rowKey="PatientID"
       dataSource={patients}
-      columns={TABLE_COLUMNS}
+    >
+      <Column onCellClick={(row: IPatient) => history.push(`/doctor/patient/${row.PatientID}`)} title="Ім'я" dataIndex="FirstName" key="FirstName" />
+      <Column onCellClick={(row: IPatient) => history.push(`/doctor/patient/${row.PatientID}`)} title="Прізвище" dataIndex="LastName" key="LastName" />
+      <Column
+        onCellClick={(values: IPatient) => handleDeletePatient(values.PatientID)}
+        title="Action"
+        key="action"
+        render={() => {
+          return <a>Delete</a>;
+        }}
     />
+    </Table>
   );
 };
 
--- src/constants/API.ts
+++ src/constants/API.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://localhost:4200/';
+export const BASE_URL = 'http://hospitalapi.dncapp.website/';
 export const CALCULATE = 'calculate';
 export const ANALIZE = 'analize';
 export const ANALIZES = 'analizes';
